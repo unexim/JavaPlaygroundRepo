5024482524481f8b61f9818bd261f6a9719a72b0
unknown <radoslav.tashev@RadoslavT-v.musala.com>
2015-12-15 11:10:19 +0200

    fixed MyLogger to give all the right output and not overwrite the output.txt file

--- internalTrainingsTask/MyLogger.java
+++ internalTrainingsTask/MyLogger.java
@@ -1,7 +1,9 @@
 package internalTrainingsTask;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class MyLogger {
 	
@@ -9,17 +11,26 @@ public class MyLogger {
 	
 	public static void log(String message) { 
 		
-		try{
-			
-			PrintWriter out = new PrintWriter("output.txt");
-			out.println(message);
-			out.close();
-		}
-		catch(IOException iox){
-			
-			iox.printStackTrace();
-		}
+		File log = new File("output.txt");
+
+				try{
+				    if(!log.exists()){
+				        System.out.println("We had to make a new file.");
+				        log.createNewFile();
+				    }
+
+				    FileWriter fileWriter = new FileWriter(log, true);
+
+				    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);				    
+				    bufferedWriter.write(message);
+				    bufferedWriter.write(System.lineSeparator());
+				    bufferedWriter.close();
+
+				    System.out.println("Done");
+				} catch(IOException e) {
+				    System.out.println("COULD NOT LOG!!");
+				}
 	    		
-	    }
-    
+	}
+	    
 }
